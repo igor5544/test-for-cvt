e4010b6aec4ed5661fd193bd0699c58ee4b0618a
igor5544 <igor5544@bk.ru>
2020-09-06 06:55:39 +0500

    init project

---
+++ gulpfile.js
@@ -0,0 +1,133 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore")
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("cssLibs", function () {
+  return gulp.src([
+    "node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css",
+    "node_modules/magnific-popup/dist/magnific-popup.css",
+  ])
+    .pipe(concat("libs.min.css"))
+    .pipe(plumber())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("build/img/webp"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename("sprite_auto.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/**/*.js")
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("jsLibs", function () {
+  return gulp.src([
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/child-replace-with-polyfill/index.js",
+    "node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js",
+    "node_modules/magnific-popup/dist/jquery.magnific-popup.min.js",
+    "node_modules/object-fit-images/dist/ofi.min.js",
+  ])
+    .pipe(concat("vendor.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source//*.ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "copy", "webp", "css", "sprite", "html", "cssLibs", "jsLibs"));
+gulp.task("start", gulp.series("build", "server"));
---
+++ source/js/main.js
@@ -0,0 +1,254 @@
+'use strict';
+
+{
+  objectFitImages();
+}
+
+{
+  const ZERO_PADDING = 0;
+
+  let htmlElement = document.querySelector('html');
+  let bodyElement = document.body;
+
+  function scrollbarWidth() {
+    let documentWidth = parseInt(document.documentElement.clientWidth);
+    let windowsWidth = parseInt(window.innerWidth);
+    let scrollbarWidth = windowsWidth - documentWidth;
+    return scrollbarWidth;
+  }
+
+  function scrollIndent() {
+    bodyElement.style.marginRight = scrollbarWidth() + 'px';
+  }
+
+  function hideScroll() {
+    htmlElement.setAttribute('style', '');
+    htmlElement.classList.add('open-popup');
+  }
+
+  function showScroll() {
+    bodyElement.style.marginRight = ZERO_PADDING;
+    htmlElement.classList.remove('open-popup');
+  }
+
+  window.popupUtiils = {
+    scrollIndent: scrollIndent,
+    hideScroll: hideScroll,
+    showScroll: showScroll,
+  };
+}
+
+{
+  const POPUP_REMOVE_DELAY_IN_MS = 300;
+  const BTN_TEXT_LOGOUT = 'Выйти';
+
+  let loginFormElement = document.querySelector('.login-form');
+  let loginFieldElement = loginFormElement.querySelector('#login');
+  let rememberFieldElement = loginFormElement.querySelector('#remember');
+  let passwordFieldElement = loginFormElement.querySelector('#password');
+  let userNameElement = document.querySelector('.header__user');
+  let loginBtnElement = document.querySelector('.header__btn');
+
+  let isStorageSupport = true;
+  let storageLogin;
+  let storageRemember;
+
+  let popupSettings = {
+    type: 'inline',
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: POPUP_REMOVE_DELAY_IN_MS,
+    mainClass: 'popup-login--zoom',
+    callbacks: {
+      beforeOpen: function () {
+        window.popupUtiils.scrollIndent();
+      },
+      open: function () {
+        window.popupUtiils.hideScroll();
+        focusField();
+      },
+      close: function () {
+        window.popupUtiils.showScroll();
+      }
+    }
+  }
+
+  $('a[href="#popup-login"').magnificPopup(popupSettings);
+  refreshStorageInfo();
+
+  function refreshStorageInfo() {
+    try {
+      storageLogin = localStorage.getItem('login');
+    } catch (err) {
+      isStorageSupport = false;
+    }
+
+    try {
+      storageRemember = localStorage.getItem('remember');
+    } catch (err) {
+      isStorageSupport = false;
+    }
+
+    if (storageLogin) {
+      loginFieldElement.value = storageLogin;
+    }
+
+    if (storageRemember == 'true' && storageLogin) {
+      userLiogIn();
+    }
+  }
+
+  function focusField() {
+    setTimeout(function () {
+      if (storageLogin) {
+        passwordFieldElement.focus();
+      } else {
+        loginFieldElement.focus();
+      }
+    }, POPUP_REMOVE_DELAY_IN_MS);
+  }
+
+  loginFormElement.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    if (isStorageSupport) {
+      localStorage.setItem('login', loginFieldElement.value);
+      localStorage.setItem('remember', rememberFieldElement.checked);
+    }
+
+    $.magnificPopup.close();
+    passwordFieldElement.value = '';
+    userLiogIn();
+  });
+
+  function userLiogIn() {
+    userNameElement.textContent = localStorage.getItem('login');
+    userNameElement.addEventListener('click', swapOnTextField);
+
+    swapBtn();
+  }
+
+  function swapBtn() {
+    let newBtn = loginBtnElement.cloneNode(true);
+
+    newBtn.classList.add('header__btn--logout');
+    newBtn.textContent = BTN_TEXT_LOGOUT;
+    newBtn.setAttribute('href', '');
+    newBtn.addEventListener('click', userLogout);
+
+    loginBtnElement.replaceWith(newBtn);
+
+    function userLogout(evt) {
+      evt.preventDefault();
+      userNameElement.textContent = '';
+
+      localStorage.setItem('remember', 'false');
+
+      newBtn.replaceWith(loginBtnElement);
+    }
+  }
+
+  function swapOnTextField() {
+    let textContent = userNameElement.textContent;
+
+    let inputTextElement = document.createElement('input');
+    inputTextElement.setAttribute('maxlength', '20');
+    inputTextElement.setAttribute('type', 'text');
+    inputTextElement.classList.add('header__user');
+    inputTextElement.classList.add('header__user--edit');
+    inputTextElement.value = textContent;
+
+    userNameElement.replaceWith(inputTextElement);
+    inputTextElement.focus();
+
+    inputTextElement.addEventListener('keydown', function (evt) {
+      if (evt.key == 'Enter') {
+        inputTextElement.blur();
+      }
+    });
+
+    inputTextElement.addEventListener('blur', function () {
+      endEdit();
+    });
+
+    function endEdit() {
+      if (inputTextElement.value > 0) {
+        userNameElement.textContent = inputTextElement.value;
+      }
+
+      if (isStorageSupport) {
+        localStorage.setItem('login', inputTextElement.value);
+      }
+
+      refreshStorageInfo();
+
+      inputTextElement.replaceWith(userNameElement);
+    }
+  }
+}
+
+{
+  const INACTIVE_TAB_INDEX = -1;
+
+  let tabBtnsElements = document.querySelectorAll('.navigation__link');
+  let tabBtnsList = Array.prototype.slice.call(tabBtnsElements);
+  let tabElements = document.querySelectorAll('.tab-item');
+  let tabList = Array.prototype.slice.call(tabElements);
+
+  tabBtnsList.forEach(function (tab, i) {
+    if (i === 0) {
+      tab.classList.add('navigation__link--active');
+      tab.tabIndex = INACTIVE_TAB_INDEX;
+    }
+
+    tab.addEventListener('click', function (evt) {
+      let activeTab = document.querySelector('.navigation__link--active');
+      evt.preventDefault();
+
+      if (tab !== activeTab) {
+        changeActiveTabLink(tab, activeTab);
+        changeActiveArticle(i);
+      }
+    });
+  });
+
+  function changeActiveTabLink(newTab, previousTab) {
+    previousTab.removeAttribute('tabIndex');
+    newTab.tabIndex = INACTIVE_TAB_INDEX;
+
+    previousTab.classList.remove('navigation__link--active');
+    newTab.classList.add('navigation__link--active');
+  }
+
+  function changeActiveArticle(i) {
+    let activeTab = document.querySelector('.tab-item--active');
+
+    activeTab.classList.add('tab-item--inactive');
+    activeTab.classList.remove('tab-item--active');
+
+    tabList[i].classList.add('tab-item--active');
+    tabList[i].classList.remove('tab-item--inactive');
+  }
+
+
+  tabList.forEach(function (tab, i) {
+    if (i === 0) {
+      tab.classList.add('tab-item--active');
+    } else {
+      tab.classList.add('tab-item--inactive');
+    }
+  });
+}
+
+{
+  let chanelListElement = document.querySelector('.channel-list');
+
+  chanelListElement.classList.add('channel-list--js');
+
+  $('.channel-list').mCustomScrollbar({
+    normalizeDelta: true,
+    theme:"inset-dark",
+  });
+}
